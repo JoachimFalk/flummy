5eb5cf9d5aa13c5d76c3b2e340c567a4dc80d063
Martin Streubuehr <streubuehr@cs.fau.de>
2008-06-30 11:16:30 +0000

    - added missing file PowerMode.h
    git-archimport-id: falk@cs.fau.de--2008-systemoc-intern/SystemC-VPC--multihop--0.6--patch-37

---
+++ PowerMode.h
@@ -0,0 +1,48 @@
+/******************************************************************************
+ *                        Copyright 2004
+ *                Lehrstuhl fuer Hardware-Software-Codesign
+ *                  Universitaet Erlangen-Nuernberg
+ *                    All rights reserved
+ *
+ * Title: SystemC-VPC
+ * Comment:
+ * ----------------------------------------------------------------------------
+ * PowerMode.h
+ * ----------------------------------------------------------------------------
+ * Modifications History:
+ * ----------------------------------------------------------------------------
+ * Notes:
+ * ----------------------------------------------------------------------------
+ * $log$
+ *****************************************************************************/
+#ifndef __INCLUDED__POWERMODE__H__
+#define __INCLUDED__POWERMODE__H__
+namespace SystemC_VPC{
+
+  /**
+   * 
+   */
+  class PowerMode
+  {
+  public:
+    PowerMode(const size_t &_mode) : mode(_mode) {}
+
+    //FIXME: needed for std::map only
+    PowerMode() : mode(0) {}
+
+    bool operator==(const PowerMode &rhs) const
+    {
+      return mode == rhs.mode;
+    }
+
+    bool operator<(const PowerMode &rhs) const
+    {
+      return mode < rhs.mode;
+    }
+
+  private:
+    size_t mode;
+  };
+
+}
+#endif // __INCLUDED__POWERMODE__H__
