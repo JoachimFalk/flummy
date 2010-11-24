e928b9da6d4ad73397ba9ea27d601f271fba7080
Martin Streubuehr <streubuehr@cs.fau.de>
2010-11-24 14:09:29 +0100

    added missing files

---
+++ ScheduledTask.cpp
@@ -0,0 +1,33 @@
+/*
+ * ScheduledTask.cpp
+ *
+ *  Created on: Nov 17, 2010
+ *      Author: streubuehr
+ */
+
+#include <systemcvpc/ScheduledTask.hpp>
+#include <systemcvpc/Delayer.hpp>
+
+namespace SystemC_VPC
+{
+
+ScheduledTask::ScheduledTask() :
+  component(NULL)
+{
+}
+
+ScheduledTask::~ScheduledTask()
+{
+}
+void ScheduledTask::setDelayer(Delayer *component)
+{
+  this->component = component;
+}
+void ScheduledTask::notifyActivation(bool active)
+{
+  if (component != NULL) { // FALLBACKMODE
+    component->notifyActivation(this, active);
+  }
+}
+
+} // namespace SystemC_VPC
