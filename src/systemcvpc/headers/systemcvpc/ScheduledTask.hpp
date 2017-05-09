74bc92799a6b48c466d88ed7b70f223892066350
Simone Müller <simone.mueller@fau.de>
2017-05-09 18:37:13 +0200

    Begin to refactor SysteMoC <-> SystemC_VPC scheduling interface.

--- src/systemcvpc/headers/systemcvpc/ScheduledTask.hpp
+++ src/systemcvpc/headers/systemcvpc/ScheduledTask.hpp
@@ -45,24 +45,67 @@ typedef size_t ProcessId;
 
 class Delayer;
 
+
+class SchedulingInterface {
+
+protected:
+  // This will execute the actor. The actor must be fireable if this method is called.
+  // This will be implemented by the SysteMoC actor and called by the scheduler.
+  virtual void schedule() = 0;
+  // This will test if the actor is fireable.
+  // This will be implemented by the SysteMoC actor and called by the scheduler.
+  virtual bool canFire()  = 0;
+
+  // If this method returns true, then SysteMoC will call
+  // setActivation to notify the SchedulingInterface
+  // that an actor is fireable. Otherwise, this has
+  // to be check via calls to canFire.
+  virtual bool useActivationCallback() const = 0;
+
+  // This will be called by SysteMoC if useActivationCallback()
+  // return true.
+  virtual void setActivation(bool activation) = 0;
+  // This must return a time until the actor is suspended
+  // due to some timing behavior. If no timing behavior is
+  // given, then this should return sc_core::sc_time_stamp().
+  // This will be implemented by the SysteMoC actor and called by the scheduler.
+  virtual sc_core::sc_time const &getNextReleaseTime() const = 0;
+
+  // setActive(false) will disable the scheduling of this actor
+  // setActive(true) will enable the scheduling of this actor
+  virtual void setActive(bool) = 0;
+  virtual bool getActive() const = 0;
+
+  virtual ~SchedulingInterface() {}
+};
+
 class ScheduledTask
+  : public sc_core::sc_module
+  , public SchedulingInterface
 {
+  friend                     class Component;
+  template<class TASKTRACER> class TtPriorityComponent;
+  template<class TASKTRACER> class TtFcfsComponent;
+
+  SC_HAS_PROCESS(ScheduledTask);
 public:
-  ScheduledTask();
+  ScheduledTask(sc_core::sc_module_name name);
   virtual ~ScheduledTask();
   void setDelayer(Delayer *component);
   Delayer* getDelayer();
   void setPid(ProcessId pid);
   ProcessId getPid() const;
-  void notifyActivation(bool active);
 
-  virtual sc_time getNextReleaseTime() {
-    return sc_time_stamp();
-  }
+  virtual sc_core::sc_time const &getNextReleaseTime() const = 0;
+
+  bool useActivationCallback() const
+    { return true; }
+
+  void setActivation(bool active);
 
   void setActive(bool a);
 
-  bool getActive(){
+  bool getActive() const {
     return active;
   }
 
@@ -70,6 +113,13 @@ private:
   Delayer *component;
   ProcessId pid;
   bool active;
+
+
+  /// The following member variable are for the fallback
+  /// case if VPC scheduling is not enabled due to missing
+  /// configuration.
+  sc_core::sc_event scheduleRequest;
+  void scheduleRequestMethod();
 };
 
 }
