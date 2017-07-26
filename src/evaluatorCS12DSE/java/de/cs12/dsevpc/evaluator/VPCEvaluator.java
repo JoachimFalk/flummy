7de0af00e7d6a4fd3ecfee5d5e6a6e2910f50a2b
Simone Müller <simone.mueller@fau.de>
2017-07-26 12:48:57 +0200

    First try at VPC evaluator.

---
+++ src/evaluatorCS12DSE/java/de/cs12/dsevpc/evaluator/VPCEvaluator.java
@@ -0,0 +1,388 @@
+package de.cs12.dsevpc.evaluator;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import org.jdom.Attribute;
+import org.jdom.DocType;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+import org.opt4j.core.Objective;
+import org.opt4j.core.Objectives;
+import org.opt4j.core.problem.Evaluator;
+
+import com.google.inject.Inject;
+
+import de.cs12.dse.model.IApplication;
+import de.cs12.dse.model.IImplementation;
+import de.cs12.dse.model.IMapping;
+import de.cs12.dse.model.Routing;
+import de.cs12.dse.model.Routings;
+import de.cs12.dse.model.components.Attributes;
+import de.cs12.dse.model.components.ICommunication;
+import de.cs12.dse.model.components.IFunction;
+import de.cs12.dse.model.components.IResource;
+import de.cs12.dse.model.components.ITask;
+import de.cs12.dse.optimization.ImplementationWrapper;
+import de.cs12.dsesgx.model.Properties;
+import de.cs12.dsesgx.model.SgxModel;
+
+
+public class VPCEvaluator implements Evaluator<ImplementationWrapper> {
+
+  Objective blub = new Objective("BLUB", Objective.Sign.MAX);
+  
+  static protected final String configFileName = "config.xml";
+
+  static protected final String dtdFileName = "cmx.dtd";
+
+  static protected final String VPCCONFIGURATION = "vpcconfiguration";
+
+  static protected final String RESOURCES = "resources";
+
+  static protected final String SCHEDULER = "scheduler";
+
+  static protected final String COMPONENT = "component";
+
+  static protected final String MAPPINGS = "mappings";
+  
+  static protected final String MAPPING = "mapping";  
+
+  static protected final String PAJE = "PAJE";
+  
+  static protected final String DISTRIBUTION = "distribution";
+
+  static protected final String TRACING = "tracing";
+
+  static protected final String TYPE = "type";
+
+  static protected final String VALUE = "value";
+
+  static protected final String NAMESTR = "name";
+
+  static protected final String ATTRIBUTE = "attribute";
+
+  static protected final String TOPOLOGY = "topology";
+
+  static protected final String PARAMETER = "parameter";
+
+//  private static final String DISTRIBUTIONS = "distributions";
+
+  protected final Properties properties;
+  private final SgxModel systemoc;
+  
+  @Inject
+  public VPCEvaluator(Properties properties, SgxModel smocModel) {
+    this.properties = properties;
+    this.systemoc = smocModel;
+
+  }
+  
+  @Override
+  public Objectives evaluate(ImplementationWrapper implementationWrapper) {
+    IImplementation<ITask, IResource, IMapping> implementation = implementationWrapper.getImplementation();
+    Objectives objectives = new Objectives();
+    if (implementation == null || !implementation.isFeasible()) {
+      objectives.add(blub, Objective.INFEASIBLE);
+      return objectives;
+    }
+    objectives.add(blub, 999);
+    Element root = new Element(VPCCONFIGURATION);
+    Element resources = processResources(implementation);
+    
+    root.addContent(resources);
+    root.addContent(processMappings(implementation));
+    
+    Map<ICommunication, List<Routing<IResource>>> oneToOneRoutings = extractRouting(implementation);
+    root.addContent(routingsToJdom(oneToOneRoutings, implementation, resources));
+
+
+    DocType vpcXml = new DocType(VPCCONFIGURATION, dtdFileName);
+    Document doc = new Document(root, vpcXml);
+
+    
+    try {
+      // generate temporary directory
+      File simDir = File.createTempFile("mjpeg", "sim");
+      simDir.delete();
+      simDir.mkdir();
+      simDir.deleteOnExit();
+      implementation.setAttribute(VPCCONFIGURATION, simDir);
+
+      // generate VPC config
+      File confFile = new File(simDir.getCanonicalPath(), configFileName);
+      BufferedWriter bw = new BufferedWriter(new FileWriter(confFile));
+
+      XMLOutputter serializer = new XMLOutputter(Format.getPrettyFormat());
+      serializer.output(doc, bw);
+      bw.close();
+    } catch (IOException e) {
+      System.err.println("Got an exception during I/O with simulation:\n" + e);
+    }
+    return objectives;
+
+  }
+
+//  /**
+//   * Add used {@link Distribution}s to the root XML {@link Element}.
+//   * 
+//   * @param root
+//   *            element to add the distributions to
+//   */
+//  private void addDistributions(Element root) {
+//    if (!this.distributions.isEmpty()) {
+//      Element element = new Element(DISTRIBUTIONS);
+//      for (Distribution distribution : this.distributions) {
+//        System.out.println("added " + distribution + " to distributions");
+//        Element d = new Element(DISTRIBUTION);
+//        d.setAttribute(NAMESTR, distribution.getId());
+//        element.addContent(d);
+//      }
+//      root.addContent(0, element);
+//    }
+//  }
+
+  private Element processResources(IImplementation<ITask, IResource, IMapping> implementation) {  
+    Element resources = new Element(RESOURCES);
+    
+    for (IResource resource : implementation.getAllocation().getVertices()) {
+      String name = resource.getAttribute("NAME");
+      String scheduler = "RoundRobin";
+      Attribute xmlAttribute = resource.getAttribute("scheduler");
+      if(xmlAttribute != null) {
+        scheduler = xmlAttribute.getValue();
+      }
+ 
+      Element component = new Element(COMPONENT);
+      component.setAttribute(NAMESTR, name);
+  
+      component.setAttribute(SCHEDULER, scheduler);
+      resources.addContent(component);
+  
+      Element tracing = new Element(ATTRIBUTE);
+      tracing.setAttribute(TYPE, TRACING);
+      tracing.setAttribute(VALUE, PAJE);
+      component.addContent(tracing);
+
+    }
+    return resources;
+  }
+  
+  private Element processMappings(IImplementation<ITask, IResource, IMapping> implementation) {
+    Element mappings = new Element("mappings");
+    de.cs12.dse.model.IApplication<ITask> application = implementation.getApplication();
+
+    for (ITask task : application.getVertices()) {
+      if (task instanceof IFunction && !(systemoc.isFifo(task))) {
+        // found ACTOR: create mapping
+        Collection<IMapping> binding = implementation.getBindings().get(task);
+        // System.err.println(task.getName());
+        for (de.cs12.dse.model.IMapping imapping : binding) {
+          // Mapping m = (Mapping) imapping;
+          IResource resource = imapping.getTarget();
+
+          // create mapping tag for CMX file
+          Element mapping = new Element("mapping");
+          mappings.addContent(mapping);
+          mapping.setAttribute("source", task.<String>getAttribute("name"));
+          mapping.setAttribute("target", resource.<String>getAttribute("NAME"));
+          
+          Attributes attributes = properties.getProperties(imapping);
+          {
+            Attributes attributes2 = imapping.getAttributes();
+            for (String attribute : attributes2.keySet()) {
+              System.out.println("map: " + attribute + " => " + imapping.getAttribute(attribute));
+            }
+          }
+          {
+            for (String attribute : attributes.keySet()) {
+              System.out.println("prop: " + attribute + " => " + attributes.get(attribute));
+            }
+          }
+          // Handle default timing
+          {
+            String vpcDelay = attributes.<String>get("delay");
+            String vpcDII   = attributes.<String>get("dii");
+            assert vpcDII == null || vpcDelay != null: "If a dii is specified, a delay must also be given!"; 
+
+            if (vpcDelay != null) {
+              Element timing = new Element("timing");
+              mapping.addContent(timing);
+              if (vpcDII != null) {
+                timing.setAttribute("latency", vpcDelay);
+                timing.setAttribute("dii", vpcDII);
+              } else {
+                timing.setAttribute("delay", vpcDelay);
+              }
+            }
+          }
+          // Handle action and guard timings
+          {
+            String vpcDelayCommaList = attributes.<String>get("vpc-function-delay");
+            String vpcDIICommaList = attributes.<String>get("vpc-function-dii");
+            
+            HashMap<String, Element> delayMap = new HashMap<String, Element>();
+            
+            if (vpcDIICommaList != null) {
+              StringTokenizer tokens = new StringTokenizer(vpcDIICommaList, " \t\n,");
+              assert tokens.countTokens() % 2 != 0
+                  : "The vpc-function-dii attribute must contain a list of comma separeted pairs of a function name and a DII timing, e.g., <fucnamea> <DIIa>, <fucnameb> <DIIb>, ...";
+              // Iterate over comma list and process DII timings
+              while (tokens.hasMoreTokens()) {
+                String fname  = tokens.nextToken();
+                String dii    = tokens.nextToken();
+                Element timing = new Element("timing");
+                timing.setAttribute("fname", fname);
+                timing.setAttribute("dii", dii);
+                assert !delayMap.containsKey(fname)
+                    : "The vpc-function-dii attribute does not permit duplicate definitions of DII values for functions, i.e., "+fname+" is defined at least twice!";
+                delayMap.put(fname, timing);
+              }
+            }
+            if (vpcDelayCommaList != null) {
+              StringTokenizer tokens = new StringTokenizer(vpcDelayCommaList, " \t\n,");
+              assert tokens.countTokens() % 2 != 0
+                  : "The vpc-function-delay attribute must contain a list of comma separeted pairs of a function name and a delay timing, e.g., <fucnamea> <delaya>, <fucnameb> <delayb>, ...";
+              // Iterate over comma list and process delay timings
+              while (tokens.hasMoreTokens()) {
+                String fname  = tokens.nextToken();
+                String delay  = tokens.nextToken();
+                Element timing = delayMap.get(fname);
+                if (timing == null) {
+                  timing = new Element("timing");
+                  timing.setAttribute("fname", fname);
+                  delayMap.put(fname, timing);
+                }
+                mapping.addContent(timing);                
+                String delayAttributeName = timing.getAttribute("dii") != null
+                    ? "latency" : "delay";
+                assert timing.getAttribute(delayAttributeName) == null
+                    : "The vpc-function-delay attribute does not permit duplicate definitions of delay values for functions, i.e., "+fname+" is defined at least twice!";
+                timing.setAttribute(delayAttributeName, delay);
+              }
+            }
+            // Paranoia
+            for (Map.Entry<String, Element> pair : delayMap.entrySet()) {
+              assert pair.getValue().getAttribute("dii") == null || pair.getValue().getAttribute("latency") != null
+                  : "The vpc-function-delay attribute must contain a delay value for a function that has a DII specified, i.e., "+pair.getKey()+" has a DII specified but no delay!"; 
+            }
+          }
+        }
+
+      } else if (systemoc.isFifo(task)) {
+        // System.err.println("Ignore Channel: " +
+        // ((Channel)task).toString()); // getName());
+      }
+    }
+    return mappings;
+  }
+ 
+  private Element routingsToJdom(Map<ICommunication, List<Routing<IResource>>> oneToOneRoutings,
+      IImplementation<ITask, IResource, IMapping> implementation, Element resources) {
+    Element topology = new Element(TOPOLOGY);
+    topology.setAttribute("tracing", "true");
+
+    Iterator<ICommunication> iter = oneToOneRoutings.keySet().iterator();
+
+    // loop over messages
+    while (iter.hasNext()) {
+      ICommunication message = iter.next();
+      Iterator<Routing<IResource>> siter = oneToOneRoutings.get(message).iterator();
+      // loop over every routing for a message (>1 if multicast)
+      // Collection<IResource> hopsUsed = new LinkedList<IResource>();
+      while (siter.hasNext()) {
+        Routing<IResource> routing = siter.next();
+        System.out.println("routing: " + routing.toString());
+        routingToJdom(implementation, resources, topology, message, routing);
+      }
+    }
+    return topology;
+  }
+
+  private void routingToJdom(IImplementation<ITask, IResource, IMapping> implementation, Element resources,
+      Element topology, ICommunication message, Routing<IResource> routing) {
+    Element route = new Element("route");    
+    topology.addContent(route);
+    
+    // find the right succ task
+    Collection<ITask> succs = implementation.getApplication().getSuccessors(message);
+    ITask successor = null;
+
+    for (ITask task : succs) {
+      System.out.println("source " + task.toString());
+      assert implementation.getBindings().get(task).size() == 1 : "Did not expect task to be bound to more than one resource";
+      IResource target = implementation.getBindings().get(task).iterator().next().getTarget();
+      System.out.println("target " + target.toString());
+      ITask predecessor = (ITask) implementation.getApplication().getPredecessors(message).toArray()[0];
+      System.out.println("Message :" +message);
+      successor = task;
+      route.setAttribute("source", predecessor.<String> getAttribute("NAME"));
+      route.setAttribute("destination", successor.<String> getAttribute("NAME"));
+
+    }
+  }
+
+  /**
+   * Extracts one to one routings from the implementation, i.e. hops on a one to one route have 0 or 1
+   * predecessor/successor. E.g. Unicast routes stay as is and multicast messages will be split into their different
+   * unicast equivalents
+   * 
+   * @param implementation
+   * @return Map with routings per message
+   */
+  private Map<ICommunication, List<Routing<IResource>>> extractRouting(
+      IImplementation<ITask, IResource, IMapping> implementation) {
+    Collection<ITask> receivingFunctions = getReceivingFunctions(implementation);
+    Map<ICommunication, List<Routing<IResource>>> oneToOneRoutings = new Hashtable<ICommunication, List<Routing<IResource>>>();
+    // traverse routing from each receiving function
+    for (ITask task : receivingFunctions) {
+      // find messages task depends on
+      Collection<ITask> messages = implementation.getApplication().getPredecessors(task);
+      for (ITask message : messages) {
+        // if message is already in 1:1 routings get List
+        List<Routing<IResource>> routingList;
+        if (oneToOneRoutings.containsKey(message)) {
+          routingList = oneToOneRoutings.get(message);
+        } else {
+          routingList = new LinkedList<Routing<IResource>>();
+          oneToOneRoutings.put((ICommunication) message, routingList);
+        }
+        // get last hop and build routing
+        // TODO: tasks are only mapped to one resource?!
+        Routing<IResource> oneToOneRouting = new Routing<IResource>();
+        routingList.add(oneToOneRouting);
+      }
+    }
+    return oneToOneRoutings;
+  }
+
+  private Collection<ITask> getReceivingFunctions(IImplementation<ITask, IResource, IMapping> implementation) {
+    IApplication<ITask> application = implementation.getApplication();
+    Collection<ITask> receivingFunctions = new ArrayList<ITask>();
+    for (ITask task : application) {
+      if (task instanceof IFunction) {
+        if (application.getInEdges(task).size() > 0) {
+          receivingFunctions.add(task);
+        }
+      }
+    }
+    return receivingFunctions;
+  }
+
+
+}
