eb6a12799e782587c51e9c3b3c66aed3f014bfc6
Joachim Falk <joachim.falk@fau.de>
2020-05-13 12:11:09 +0200

    Begin porting of VPCEvaluator to OpenDSE.

--- src/systemcvpc/cpp/detail/PreemptiveScheduler/PriorityScheduler.hpp
+++ src/systemcvpc/cpp/detail/PreemptiveScheduler/PriorityScheduler.hpp
@@ -1,8 +1,15 @@
-// -*- tab-width:8; intent-tabs-mode:nil; c-basic-offset:2; -*-
-// vim: set sw=2 ts=8 et:
+// -*- tab-width:8; indent-tabs-mode:nil; c-basic-offset:2; -*-
+// vim: set sw=2 ts=8 sts=2 et:
 /*
- * Copyright (c) 2004-2016 Hardware-Software-CoDesign, University of
- * Erlangen-Nuremberg. All rights reserved.
+ * Copyright (c)
+ *   2010 FAU -- Joachim Falk <joachim.falk@fau.de>
+ *   2010 FAU -- Martin Streubuehr <martin.streubuehr@fau.de>
+ *   2011 FAU -- Martin Streubuehr <martin.streubuehr@fau.de>
+ *   2016 FAU -- Joachim Falk <joachim.falk@fau.de>
+ *   2017 FAU -- Joachim Falk <joachim.falk@fau.de>
+ *   2017 FAU -- Simone Müller <simone.mueller@fau.de>
+ *   2018 FAU -- Joachim Falk <joachim.falk@fau.de>
+ *   2020 FAU -- Joachim Falk <joachim.falk@fau.de>
  * 
  *   This library is free software; you can redistribute it and/or modify it under
  *   the terms of the GNU Lesser General Public License as published by the Free
@@ -17,21 +24,6 @@
  *   You should have received a copy of the GNU Lesser General Public License
  *   along with this library; if not, write to the Free Software Foundation, Inc.,
  *   59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
- * 
- * --- This software and any associated documentation is provided "as is"
- * 
- * IN NO EVENT SHALL HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN NUREMBERG
- * BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR
- * CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
- * DOCUMENTATION, EVEN IF HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN
- * NUREMBERG HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
- * HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN NUREMBERG, SPECIFICALLY
- * DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED
- * HEREUNDER IS ON AN "AS IS" BASIS, AND HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF
- * ERLANGEN NUREMBERG HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
- * ENHANCEMENTS, OR MODIFICATIONS.
  */
 
 #ifndef _INCLUDED_SYSTEMCVPC_DETAIL_PREEMPTIVESCHEDULER_PRIORITYSCHEDULER_HPP
