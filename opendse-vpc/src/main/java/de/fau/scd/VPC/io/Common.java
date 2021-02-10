JoachimFalk/flummy

commit 52d47d237bddb261174ae45a3d38c9ceb93a875e

Added attribute parsing support to VPC configuration importer.

opendse-vpc/src/main/java/de/fau/scd/VPC/io/Common.java
@@ -1,0 +1,134 @@
+// -*- tab-width:8; indent-tabs-mode:nil; c-basic-offset:4; -*-
+// vim: set sw=4 ts=8 sts=4 et:
+/*
+ * Copyright (c)
+ *   2020 FAU -- Joachim Falk <joachim.falk@fau.de>
+ * 
+ *   This library is free software; you can redistribute it and/or modify it under
+ *   the terms of the GNU Lesser General Public License as published by the Free
+ *   Software Foundation; either version 2 of the License, or (at your option) any
+ *   later version.
+ * 
+ *   This library is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *   FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ *   details.
+ * 
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this library; if not, write to the Free Software Foundation, Inc.,
+ *   59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
+package de.fau.scd.VPC.io;
+
+import java.util.Iterator;
+
+public class Common {
+
+    /**
+     * Gets an iterable list of child elements named {@param childName} of the
+     * parent element {@param parentElement}.
+     *
+     * @param parentElement
+     *            the parent element
+     * @param childName
+     *            the tag name of the desired child elements
+     * @return the iterable element objects
+     */
+    public static Iterable<org.w3c.dom.Element> childElements(final org.w3c.dom.Element parentElement, final String childName) {
+        return new Iterable<org.w3c.dom.Element>() {
+
+            @Override
+            public Iterator<org.w3c.dom.Element> iterator() {
+                return new Iterator<org.w3c.dom.Element>() {
+                    private int c = -1;
+                    private final org.w3c.dom.NodeList nodes = parentElement.getChildNodes();
+
+                    {
+                        skip();
+                    }
+
+                    private int skip() {
+                        int old = c++;
+                        while (hasNext() && !nodes.item(c).getNodeName().equals(childName))
+                            ++c;
+                        return old;
+                    }
+
+                    @Override
+                    public boolean hasNext() {
+                        return nodes.getLength() > c;
+                    }
+
+                    @Override
+                    public org.w3c.dom.Element next() {
+                        return (org.w3c.dom.Element) nodes.item(skip());
+                    }
+
+                    @Override
+                    public void remove() {
+                        throw new RuntimeException("invalid operation: remove");
+                    }
+                };
+            }
+        };
+    }
+
+    /**
+     * Gets the single child element named {@param childName} of the parent
+     * element {@param parentElement}. If there are more than one or no child
+     * elements with the requested name, an exception is thrown.
+     *
+     * @param parentElement
+     *            the parent element
+     * @param childName
+     *            the tag name of the desired child elements
+     * @return the desired child element
+     * @throws SNGFormatErrorException
+     */
+    public static org.w3c.dom.Element childElement(final org.w3c.dom.Element parentElement, final String childName)
+            throws FormatErrorException {
+        return childElement(parentElement, childName, false);
+    }
+
+    /**
+     * Gets the single child element named {@param childName} of the parent
+     * element {@param parentElement}. If there are more than one child element
+     * with the requested name, an exception is thrown. If the element is
+     * optional {@param optional} and not present, then null is returned.
+     * Otherwise, if not optional and missing an exception is thrown.
+     *
+     * @param parentElement
+     *            the parent element
+     * @param childName
+     *            the tag name of the desired child elements
+     * @param optional
+     *            If true, the element is allowed to be missing
+     * @return the desired child element or null if optional and the element is
+     *         missing
+     * @throws SNGFormatErrorException
+     */
+    public static org.w3c.dom.Element childElement(final org.w3c.dom.Element parentElement, final String childName,
+            boolean optional) throws FormatErrorException {
+        Iterator<org.w3c.dom.Element> iter = childElements(parentElement, childName).iterator();
+        if (!iter.hasNext()) {
+            if (!optional)
+                throw new FormatErrorException(
+                        "Parent element " + parentElement + " is missing a " + childName + " child element!");
+            else
+                return null;
+        }
+        org.w3c.dom.Element retval = iter.next();
+        if (iter.hasNext())
+            throw new FormatErrorException(
+                    "Parent element " + parentElement + " must only have one " + childName + " child element!");
+        return retval;
+    }
+
+    static public class FormatErrorException extends Exception {
+        private static final long serialVersionUID = 3741956271022484454L;
+
+        public FormatErrorException(String message) {
+            super(message);
+        }
+    }
+}

opendse-vpc/src/main/java/de/fau/scd/VPC/helper/Common.java
@@ -1,171 +1,0 @@
-// -*- tab-width:8; indent-tabs-mode:nil; c-basic-offset:4; -*-
-// vim: set sw=4 ts=8 sts=4 et:
-/*
- * Copyright (c)
- *   2020 FAU -- Joachim Falk <joachim.falk@fau.de>
- * 
- *   This library is free software; you can redistribute it and/or modify it under
- *   the terms of the GNU Lesser General Public License as published by the Free
- *   Software Foundation; either version 2 of the License, or (at your option) any
- *   later version.
- * 
- *   This library is distributed in the hope that it will be useful, but WITHOUT
- *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- *   FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
- *   details.
- * 
- *   You should have received a copy of the GNU Lesser General Public License
- *   along with this library; if not, write to the Free Software Foundation, Inc.,
- *   59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
- */
-package de.fau.scd.VPC.helper;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-
-import net.sf.opendse.model.Attributes;
-import net.sf.opendse.model.Communication;
-import net.sf.opendse.model.Dependency;
-import net.sf.opendse.model.IAttributes;
-import net.sf.opendse.model.Link;
-import net.sf.opendse.model.Mapping;
-import net.sf.opendse.model.Resource;
-import net.sf.opendse.model.Task;
-import net.sf.opendse.model.parameter.ParameterRange;
-import net.sf.opendse.model.parameter.ParameterRangeDiscrete;
-import net.sf.opendse.model.parameter.ParameterSelect;
-import net.sf.opendse.model.parameter.ParameterUniqueID;
-
-import org.apache.commons.collections15.BidiMap;
-import org.apache.commons.collections15.bidimap.DualHashBidiMap;
-
-/**
- * The {@code Common} class contains common methods for reading and writing a
- * {@code Specification}.
- *
- * @author Martin Lukasiewycz
- *
- */
-public class Common {
-
-    public static BidiMap<String, Class<?>> classMap = new DualHashBidiMap<String, Class<?>>();
-
-    static {
-        classMap.put("INT", Integer.class);
-        classMap.put("DOUBLE", Double.class);
-        classMap.put("STRING", String.class);
-        classMap.put("BOOL", Boolean.class);
-        classMap.put("RANGE", ParameterRange.class);
-        classMap.put("DISCRETERANGE", ParameterRangeDiscrete.class);
-        classMap.put("SELECT", ParameterSelect.class);
-        classMap.put("UID", ParameterUniqueID.class);
-        classMap.put("resource", Resource.class);
-        classMap.put("link", Link.class);
-        classMap.put("task", Task.class);
-        classMap.put("communication", Communication.class);
-        classMap.put("dependency", Dependency.class);
-        classMap.put("mapping", Mapping.class);
-        classMap.put("MAP", HashMap.class);
-        classMap.put("SET", HashSet.class);
-        classMap.put("LIST", ArrayList.class);
-    }
-
-    protected static Set<Class<?>> primitives = new HashSet<Class<?>>();
-
-    static {
-        primitives.add(Boolean.class);
-        primitives.add(Byte.class);
-        primitives.add(Integer.class);
-        primitives.add(Character.class);
-        primitives.add(Short.class);
-        primitives.add(Float.class);
-        primitives.add(Long.class);
-        primitives.add(Double.class);
-    }
-
-    protected static boolean isPrimitive(Class<?> cls) {
-        return cls.isPrimitive() || primitives.contains(cls);
-    }
-
-    protected static String getType(Class<?> clazz) {
-        if (classMap.containsValue(clazz)) {
-            return classMap.getKey(clazz);
-        } else {
-            return clazz.getCanonicalName().toString();
-        }
-    }
-
-    @SuppressWarnings("rawtypes")
-    public static Object toInstance(String value, Class<?> clazz) throws IllegalArgumentException, SecurityException,
-            InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
-        if (!clazz.isEnum()) {
-            Constructor constructor = clazz.getConstructor(String.class);
-            if (constructor != null) {
-                return constructor.newInstance(value.trim());
-            }
-        } else {
-            Class<? extends Enum> eclazz = clazz.asSubclass(Enum.class);
-            for (Enum e : eclazz.getEnumConstants()) {
-                if (e.name().equalsIgnoreCase(value.trim())) {
-                    return e;
-                }
-            }
-        }
-        return null;
-    }
-
-    public static void setAttributes(IAttributes e, Attributes attributes) {
-        for (String name : attributes.keySet()) {
-            e.setAttribute(name, attributes.get(name));
-        }
-    }
-
-    /**
-     * Transforms a Base64 string into an object.
-     *
-     * @param s
-     *            the string
-     * @return the object
-     * @throws IOException
-     *             thrown in case of an IO error
-     * @throws ClassNotFoundException
-     *             thrown in case the class does not exist
-     */
-    public static Object fromString(String s) throws IOException, ClassNotFoundException {
-        byte[] data = Base64Coder.decode(s);
-        ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
-        Object o = ois.readObject();
-        ois.close();
-        return o;
-    }
-
-    /**
-     * Transforms an object into a Base64 string.
-     *
-     * @param o
-     *            the object
-     * @return the string
-     * @throws IOException
-     *             thrown in case of an IO error
-     */
-    public static String toString(Serializable o) throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ObjectOutputStream oos = new ObjectOutputStream(baos);
-        oos.writeObject(o);
-        oos.close();
-
-        return new String(Base64Coder.encode(baos.toByteArray()));
-
-    }
-
-}

opendse-vpc/src/main/java/de/fau/scd/VPC/evaluation/VPCEvaluator.java
@@ -58,7 +58,8 @@ import de.fau.scd.VPC.evaluation.VPCEvaluatorModule.TraceTypeEnum;
 import de.fau.scd.VPC.helper.ObjectiveInfo;
 import de.fau.scd.VPC.helper.TempDirectoryHandler;
 import de.fau.scd.VPC.io.VPCConfigReader;
-import de.fau.scd.VPC.io.VPCConfigReader.VPCFormatErrorException;
+import de.fau.scd.VPC.io.Common.FormatErrorException;
+
 import net.sf.opendse.model.Application;
 import net.sf.opendse.model.Architecture;
 import net.sf.opendse.model.Attributes;
@@ -189,7 +190,7 @@ public class VPCEvaluator implements ImplementationEvaluator {
       , @VPCConfigTemplate String vpcConfigTemplate
       ) throws
         FileNotFoundException
-      , VPCFormatErrorException
+      , FormatErrorException
     {
         this.simulatorExecutable    = simulatorExecutable;
         this.simulatorEnvironment   = simulatorEnvironment;
@@ -217,7 +218,7 @@ public class VPCEvaluator implements ImplementationEvaluator {
             processResources(implementation, eResources);
             org.w3c.dom.Element eMappings = VPCConfigReader.childElement(eVPCConfig, "mappings");
             processMappings(implementation, eMappings);
-        } catch (VPCFormatErrorException ex) {
+        } catch (FormatErrorException ex) {
             ex.printStackTrace();
             infeasible = true;
         }

opendse-vpc/src/main/java/de/fau/scd/VPC/io/AttributeHelper.java
@@ -1,0 +1,322 @@
+// -*- tab-width:8; indent-tabs-mode:nil; c-basic-offset:4; -*-
+// vim: set sw=4 ts=8 sts=4 et:
+/*
+ * Copyright (c)
+ *   2020 FAU -- Joachim Falk <joachim.falk@fau.de>
+ * 
+ *   This library is free software; you can redistribute it and/or modify it under
+ *   the terms of the GNU Lesser General Public License as published by the Free
+ *   Software Foundation; either version 2 of the License, or (at your option) any
+ *   later version.
+ * 
+ *   This library is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *   FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ *   details.
+ * 
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this library; if not, write to the Free Software Foundation, Inc.,
+ *   59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
+package de.fau.scd.VPC.io;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import net.sf.opendse.model.Attributes;
+import net.sf.opendse.model.Communication;
+import net.sf.opendse.model.Dependency;
+import net.sf.opendse.model.IAttributes;
+import net.sf.opendse.model.Link;
+import net.sf.opendse.model.Mapping;
+import net.sf.opendse.model.Resource;
+import net.sf.opendse.model.Task;
+import net.sf.opendse.model.parameter.ParameterRange;
+import net.sf.opendse.model.parameter.ParameterRangeDiscrete;
+import net.sf.opendse.model.parameter.ParameterSelect;
+import net.sf.opendse.model.parameter.ParameterUniqueID;
+
+import org.apache.commons.collections15.BidiMap;
+import org.apache.commons.collections15.bidimap.DualHashBidiMap;
+
+/**
+ * The {@code AttributeHelper} class contains common methods for reading and writing
+ * {@code Attributes}.
+ *
+ * @author Joachim Falk
+ */
+class AttributeHelper {
+
+    public static <E extends IAttributes> void addAttributes(org.w3c.dom.Element eElement, E element) {
+        Attributes attributes = new Attributes();
+        for (org.w3c.dom.Element eAttribute : Common.childElements(eElement, "attribute")) {
+            String name  = "vpc-" + eAttribute.getAttribute("type");
+            Object value = eAttribute.getAttribute("value");
+            attributes.put(name, value);
+        }
+        for (org.w3c.dom.Element eAttribute : Common.childElements(eElement, "opendseattr")) {
+            org.w3c.dom.Attr name = eAttribute.getAttributeNode("name");
+            if (name == null) {
+                throw new IllegalArgumentException("no name given for attribute " + eAttribute);
+            }
+            Object value = toAttribute(eAttribute);
+            attributes.put(name.getValue(), value);
+        }
+        for (String name : attributes.keySet()) {
+            element.setAttribute(name, attributes.get(name));
+        }
+    }
+
+    protected static Object toAttribute(org.w3c.dom.Element eAttribute) {
+        org.w3c.dom.Attr type = eAttribute.getAttributeNode("type");
+        org.w3c.dom.Attr javaType = eAttribute.getAttributeNode("javaType");
+        org.w3c.dom.Attr value = eAttribute.getAttributeNode("value");
+
+        Class<?> clazz = null;
+        if (javaType != null) {
+            try {
+                clazz = Class.forName(javaType.getValue());
+            } catch (ClassNotFoundException e) {}
+            if (clazz == null)
+                throw new IllegalArgumentException("Unknown opendseattr javaType " + javaType.getValue() + "!");
+        } else if (type != null) {
+            clazz =  AttributeHelper.classMap.get(type.getValue());
+            if (clazz == null)
+                throw new IllegalArgumentException("Unknown opendseattr type " + type.getValue() + "!");
+        } else {
+            throw new IllegalArgumentException("No type given for attribute " + eAttribute);
+        }
+        {
+            if (Map.class.isAssignableFrom(clazz)) {
+                return toAttributeMap(eAttribute, clazz);
+            } else if (Collection.class.isAssignableFrom(clazz)) {
+                return toAttributeCollection(eAttribute, clazz);
+            } else {
+                if (value == null) {
+                    throw new IllegalArgumentException("no value given for attribute " + eAttribute);
+                }
+                return toAttributeObject(eAttribute, clazz, value.getValue());
+            }
+        }
+    }
+
+    /**
+     * Constructs an attribute collection that contains all passed elements and
+     * their corresponding class.
+     *
+     * @param eAttribute
+     *            the attribute element to add the collection to
+     * @param clazz
+     *            the class of the objects that are to create
+     * @return the constructed collection
+     */
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    protected static Object toAttributeMap(org.w3c.dom.Element eAttribute, Class<?> clazz) {
+        Map map;
+        try {
+            map = (Map) clazz.getConstructor().newInstance();
+        } catch (NoSuchMethodException | SecurityException |
+                 InstantiationException | IllegalAccessException |
+                 IllegalArgumentException | InvocationTargetException e) {
+            throw new IllegalArgumentException("type value mismatch for attribute " + eAttribute);
+        }
+        for (org.w3c.dom.Element eNestedAttribute : Common.childElements(eAttribute, "opendseattr")) {
+            org.w3c.dom.Attr name = eNestedAttribute.getAttributeNode("name");
+            if (name == null) {
+                throw new IllegalArgumentException("no name given for attribute " + eAttribute);
+            }
+            Object value = toAttribute(eNestedAttribute);
+            map.put(name.getValue(), value);
+        }
+        return map;
+    }
+
+    /**
+     * Constructs an attribute collection that contains all passed elements and
+     * their corresponding class.
+     *
+     * @param eAttribute
+     *            the attribute element to add the collection to
+     * @param clazz
+     *            the class of the objects that are to create
+     * @return the constructed collection
+     */
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    protected static Object toAttributeCollection(org.w3c.dom.Element eAttribute, Class<?> clazz) {
+        Collection collectionAttribute;
+        try {
+            collectionAttribute = (Collection) clazz.getConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+                | NoSuchMethodException | SecurityException e) {
+            throw new IllegalArgumentException("type value mismatch for attribute " + eAttribute);
+        }
+        for (org.w3c.dom.Element eNestedAttribute : Common.childElements(eAttribute, "opendseattr")) {
+            Object actualEntry = toAttribute(eNestedAttribute);
+            collectionAttribute.add(actualEntry);
+        }
+        return collectionAttribute;
+    }
+
+    /**
+     * Constructs an instance of the passed class that contains the passed
+     * value.
+     *
+     * @param eAttribute
+     *            the XML attribute to convert
+     * @param clazz
+     *            the class of the object that is to create
+     * @param value
+     *            the value of the object that is to create
+     * @return the constructed object
+     */
+    protected static Object toAttributeObject(org.w3c.dom.Element eAttribute, Class<?> clazz, String value) {
+        Object object = null;
+
+        {
+            if (clazz.equals(Boolean.class)) {
+                if (value.equals("0")) {
+                        value = "false";
+                } else if (value.equals("1")) {
+                        value = "true";
+                }
+            }
+            try {
+                object = AttributeHelper.toInstance(value, clazz);
+            } catch (IllegalArgumentException | SecurityException | InstantiationException | IllegalAccessException
+                    | InvocationTargetException | NoSuchMethodException e) {
+            }
+            // "fallback procedure"
+            if (object == null && clazz.equals(Serializable.class)) {
+                try {
+                    object = AttributeHelper.fromString(value);
+                } catch (IOException | ClassNotFoundException e) {
+                }
+            }
+        }
+        if (object == null) {
+            throw new IllegalArgumentException("type value mismatch for attribute " + eAttribute);
+        }
+        return object;
+    }
+
+    protected static BidiMap<String, Class<?>> classMap = new DualHashBidiMap<String, Class<?>>();
+
+    static {
+        classMap.put("INT", Integer.class);
+        classMap.put("DOUBLE", Double.class);
+        classMap.put("STRING", String.class);
+        classMap.put("BOOL", Boolean.class);
+        classMap.put("RANGE", ParameterRange.class);
+        classMap.put("DISCRETERANGE", ParameterRangeDiscrete.class);
+        classMap.put("SELECT", ParameterSelect.class);
+        classMap.put("UID", ParameterUniqueID.class);
+        classMap.put("resource", Resource.class);
+        classMap.put("link", Link.class);
+        classMap.put("task", Task.class);
+        classMap.put("communication", Communication.class);
+        classMap.put("dependency", Dependency.class);
+        classMap.put("mapping", Mapping.class);
+        classMap.put("MAP", HashMap.class);
+        classMap.put("SET", HashSet.class);
+        classMap.put("LIST", ArrayList.class);
+    }
+
+    protected static Set<Class<?>> primitives = new HashSet<Class<?>>();
+
+    static {
+        primitives.add(Boolean.class);
+        primitives.add(Byte.class);
+        primitives.add(Integer.class);
+        primitives.add(Character.class);
+        primitives.add(Short.class);
+        primitives.add(Float.class);
+        primitives.add(Long.class);
+        primitives.add(Double.class);
+    }
+
+    protected static boolean isPrimitive(Class<?> cls) {
+        return cls.isPrimitive() || primitives.contains(cls);
+    }
+
+    protected static String getType(Class<?> clazz) {
+        if (classMap.containsValue(clazz)) {
+            return classMap.getKey(clazz);
+        } else {
+            return clazz.getCanonicalName().toString();
+        }
+    }
+
+    @SuppressWarnings("rawtypes")
+    protected static Object toInstance(String value, Class<?> clazz) throws IllegalArgumentException, SecurityException,
+            InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        if (!clazz.isEnum()) {
+            Constructor constructor = clazz.getConstructor(String.class);
+            if (constructor != null) {
+                return constructor.newInstance(value.trim());
+            }
+        } else {
+            Class<? extends Enum> eclazz = clazz.asSubclass(Enum.class);
+            for (Enum e : eclazz.getEnumConstants()) {
+                if (e.name().equalsIgnoreCase(value.trim())) {
+                    return e;
+                }
+            }
+        }
+        return null;
+    }
+
+    protected static void setAttributes(IAttributes e, Attributes attributes) {
+        for (String name : attributes.keySet()) {
+            e.setAttribute(name, attributes.get(name));
+        }
+    }
+
+    /**
+     * Transforms a Base64 string into an object.
+     *
+     * @param s
+     *            the string
+     * @return the object
+     * @throws IOException
+     *             thrown in case of an IO error
+     * @throws ClassNotFoundException
+     *             thrown in case the class does not exist
+     */
+    public static Object fromString(String s) throws IOException, ClassNotFoundException {
+        byte[] data = Base64Coder.decode(s);
+        ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
+        Object o = ois.readObject();
+        ois.close();
+        return o;
+    }
+
+    /**
+     * Transforms an object into a Base64 string.
+     *
+     * @param o
+     *            the object
+     * @return the string
+     * @throws IOException
+     *             thrown in case of an IO error
+     */
+    public static String toString(Serializable o) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(o);
+        oos.close();
+
+        return new String(Base64Coder.encode(baos.toByteArray()));
+    }
+}

commit 79f5c4336126c1022dea2779d66e596c44fff5cf

Fixed bug in VPCEvaluator introduced by commit 52d47d237bddb261174ae45a3d38c9ceb93a875e (Added attribute parsing support to VPC configuration importer).

opendse-vpc/src/main/java/de/fau/scd/VPC/io/Common.java
@@ -3,6 +3,7 @@
 /*
  * Copyright (c)
  *   2020 FAU -- Joachim Falk <joachim.falk@fau.de>
+ *   2021 FAU -- Joachim Falk <joachim.falk@fau.de>
  * 
  *   This library is free software; you can redistribute it and/or modify it under
  *   the terms of the GNU Lesser General Public License as published by the Free
@@ -40,6 +41,7 @@ public class Common {
             @Override
             public Iterator<org.w3c.dom.Element> iterator() {
                 return new Iterator<org.w3c.dom.Element>() {
+                    private org.w3c.dom.Element cur = null;
                     private int c = -1;
                     private final org.w3c.dom.NodeList nodes = parentElement.getChildNodes();
 
@@ -61,12 +63,16 @@ public class Common {
 
                     @Override
                     public org.w3c.dom.Element next() {
-                        return (org.w3c.dom.Element) nodes.item(skip());
+                        cur = (org.w3c.dom.Element) nodes.item(skip());
+                        return cur;
                     }
 
                     @Override
                     public void remove() {
-                        throw new RuntimeException("invalid operation: remove");
+                        if (cur == null)
+                            throw new IllegalStateException();
+                        parentElement.removeChild(cur);
+                        --c;
                     }
                 };
             }